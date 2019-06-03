0d42a9f23d9cce20cb8d563156683c0830709a5e
Ashirwad Sangwan <43483454+ashirwadsangwan@users.noreply.github.com>
2019-06-03 12:53:10 +0530

    Day03

---
+++ 30DaysOfCode/introToConditionalStatements.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    N = int(input())
+    if N%2 != 0:
+        print('Weird')
+    elif (N%2==0) and (N>=2) and (N<=5):
+        print('Not Weird')
+    elif (N%2==0) and (N>=6) and (N<=20):
+        print('Weird')
+    else:
+        print('Not Weird')
