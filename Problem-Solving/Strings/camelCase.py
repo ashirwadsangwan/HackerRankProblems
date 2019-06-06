d094e82d7b5d5a1a87877f9b43f5f098e8b94301
Ashirwad Sangwan <43483454+ashirwadsangwan@users.noreply.github.com>
2019-06-06 18:21:26 +0530

    Problem 2

---
+++ Problem-Solving/Strings/camelCase.py
@@ -0,0 +1,18 @@
+
+
+import re
+
+def camelcase(s):
+    return len(re.findall('[A-Z][^A-Z]*', s))+1
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = camelcase(s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
