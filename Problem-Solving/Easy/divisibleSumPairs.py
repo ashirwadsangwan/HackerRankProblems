696cd0eca6dcaf40694d1619aeb510a5bdff1132
Ashirwad Sangwan <43483454+ashirwadsangwan@users.noreply.github.com>
2019-06-07 19:47:42 +0530

    Problem 11

---
+++ Problem-Solving/Easy/divisibleSumPairs.py
@@ -0,0 +1,24 @@
+def divisibleSumPairs(n, k, ar):
+    pairs = 0
+    for i in range(n):
+        for j in range(i+1,n):
+            if (ar[i]+ar[j])%k==0:
+                pairs+=1
+    return pairs
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = divisibleSumPairs(n, k, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
