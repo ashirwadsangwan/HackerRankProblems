6d03dd5866ff948d61947fd5a46f8638a93c45dd
Ashirwad Sangwan <43483454+ashirwadsangwan@users.noreply.github.com>
2019-06-08 23:23:10 +0530

    Problem 2

---
+++ Python/02python-If-Else.py
@@ -0,0 +1,11 @@
+N = int(input())
+
+if N%2 != 0:
+    print('Weird')
+if N%2 == 0:
+    if N>=2 and N<=5:
+        print('Not Weird')
+    elif N>=6 and N<=20:
+        print('Weird')
+    else:
+        print('Not Weird')
