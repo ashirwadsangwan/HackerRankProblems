0b5f7e5198a9317d9bcc50d22b7a5ba10483c145
Ashirwad Sangwan <43483454+ashirwadsangwan@users.noreply.github.com>
2019-06-08 23:39:48 +0530

    Problem 6

---
+++ Python/06isLeap.py
@@ -0,0 +1,18 @@
+def is_leap(year):
+    
+    if year%4==0 and year%100 != 0:
+        return True
+    elif year%4==0 and year%100 ==0:
+        if year%400==0:
+            return True
+        else:
+            return False
+    else:
+        return False
+    
+   
+    
+    
+
+year = int(input())
+print(is_leap(year))
