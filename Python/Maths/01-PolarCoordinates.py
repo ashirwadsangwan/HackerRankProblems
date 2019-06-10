6423d941b535960befe03f211be5dbeec7962b1d
Ashirwad Sangwan <43483454+ashirwadsangwan@users.noreply.github.com>
2019-06-10 15:00:53 +0530

    Problem 1

---
+++ Python/Maths/01-PolarCoordinates.py
@@ -0,0 +1,4 @@
+import cmath
+z = complex(input()) # complex converts a number into complex number
+print(abs(z)) .      ## abs function here return the absolute value of the complex number which is given by the square root of sum of sqaures of real and imaginary parts of the complex numbers.
+print(cmath.phase(z)) ## cmath.phase returns the argument or the angle theta that the the real and imaginary number make with each other.
