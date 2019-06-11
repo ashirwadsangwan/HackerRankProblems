484879e3107ec4bae22787a14a48bd34831c957e
Ashirwad Sangwan <43483454+ashirwadsangwan@users.noreply.github.com>
2019-06-11 16:39:56 +0530

    Basic Probability

---
+++ 10DaysOfStatistics/Day020.py
@@ -0,0 +1,5 @@
+#In a single toss of 2 fair (evenly-weighted) six-sided dice, find the probability that their sum will be at most 9.
+
+total_chances = 36
+sum_at_most_nine = 30
+print(sum_at_most_nine/total_chances)
