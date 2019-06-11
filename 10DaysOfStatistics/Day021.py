444b2610b1c5f5cb9b51bcddec3d1a301f0a2913
Ashirwad Sangwan <43483454+ashirwadsangwan@users.noreply.github.com>
2019-06-11 16:41:50 +0530

    More Dice

---
+++ 10DaysOfStatistics/Day021.py
@@ -0,0 +1,7 @@
+## In a single toss of 2 fair (evenly-weighted) six-sided dice, find the probability that the values rolled by each die will be different and the two dice have a sum of 6.
+
+favouble = 4
+total  = 36
+print(favourable/total)
+
+
