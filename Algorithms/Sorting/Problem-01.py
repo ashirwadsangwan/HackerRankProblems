bc85fd5128610d73fb448a3be1bd910f56bdf8af
Ashirwad Sangwan <43483454+ashirwadsangwan@users.noreply.github.com>
2019-06-18 17:38:10 +0530

    Big Sorting

---
+++ Algorithms/Sorting/Problem-01.py
@@ -0,0 +1,24 @@
+def bigSorting(unsorted):
+    
+    array = sorted(sorted(unsorted), key = len)
+    return [i for i in array]
+    
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    unsorted = []
+
+    for _ in range(n):
+        unsorted_item = input()
+        unsorted.append(unsorted_item)
+
+    result = bigSorting(unsorted)
+
+    fptr.write('\n'.join(result))
+    fptr.write('\n')
+
+    fptr.close()
