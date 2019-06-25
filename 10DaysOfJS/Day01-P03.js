b0eea8b81d3aa5eaa59da5d88d626469b0df0273
Ashirwad Sangwan <43483454+ashirwadsangwan@users.noreply.github.com>
2019-06-25 15:28:55 +0530

    Let and Const

---
+++ 10DaysOfJS/Day01-P03.js
@@ -0,0 +1,14 @@
+function main() {
+
+    const PI = Math.PI
+    let r = readLine()
+
+    // area = 2*PI*r
+
+    console.log(PI * r**2)
+    
+
+    // parameter = 2*PI*r
+
+    console.log(2*PI * r )
+}
